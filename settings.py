d667894f6bed4cf89b93e04851f9a518620a8269
KarabakhTag <106745732+KarabakhTag@users.noreply.github.com>
2022-06-12 13:45:46 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # TheOksigen
-TOKEN = "1862890593:AAGXXyk3v1bVcg4bOtYXHzJKG2JhQZ_1NWk"
+TOKEN = "5300192410:AAE4pl7RfEH1jDuzHh4bee_b5h5M-qLYEcU"
 with open('soz.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
